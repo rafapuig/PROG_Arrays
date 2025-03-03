4d0774daf8b5a4d35834f9819c5b0bd801288d95
Rafa Puig <rafapuig.profe@gmail.com>
2025-03-03 01:08:18 +0100

    domino

---
+++ src/exercises/model/Domino.java
@@ -0,0 +1,132 @@
+package exercises.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class Domino {
+
+    private final int top;
+    private final int bottom;
+
+    public Domino(int top, int bottom) {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    boolean contains(int number) {
+        return number == top || number == bottom;
+    }
+
+    Domino flip() {
+        return flip(this);
+    }
+
+    public static Domino flip(Domino domino) {
+        return new Domino(domino.bottom, domino.top);
+    }
+
+
+    private static String toDots(int number) {
+        return switch (number) {
+            case 1 -> " ∙ ";
+            case 2 -> " ̇ .";
+            case 3 -> "˙∙.";
+            case 4 -> ": :";
+            case 5 -> ":∙:";
+            case 6 -> ":::";
+            case 0 -> "   ";
+            default -> " " + number + " ";
+        };
+    }
+
+    private static String sideToString(int number) {
+        //return sideValue != 0 ? String.valueOf(sideValue) : " ";
+        return toDots(number);
+    }
+
+    @Override
+    public final boolean equals(Object other) {
+        if (!(other instanceof Domino domino)) return false;
+        if(hashCode() != domino.hashCode()) return false;
+        return this.top == domino.top && this.bottom == domino.bottom
+                || this.top == domino.bottom && this.bottom == domino.top;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = top;
+        result = 31 * result + bottom;
+        return result;
+    }
+
+    private static final char SURROGATE_PAIR_HIGH = '\uD83C';
+    private static final char SURROGATE_PAIR_LOW_HORIZONTAL = '\uDC31';
+    private static final char SURROGATE_PAIR_LOW_VERTICAL = '\uDC63';
+
+    String toChar(boolean vertical) {
+        char surrogatePairLowBase = vertical ? SURROGATE_PAIR_LOW_VERTICAL : SURROGATE_PAIR_LOW_HORIZONTAL;
+        int offset = bottom + top * 7;
+        char surrogatePairLow = (char) (surrogatePairLowBase + offset);
+        return String.format("%s%s", SURROGATE_PAIR_HIGH, surrogatePairLow);
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + sideToString(top) + "|" + sideToString(bottom) + "]";
+    }
+}
+
+class DominoesTest {
+
+    public static void main(String[] args) {
+
+        testFlip();
+        testEquals();
+
+        List<Domino> dominoList = new ArrayList<>();
+        for (int i = 0; i <= 6; i++) {
+            for (int j = 0; j <= 6; j++) {
+                dominoList.add(new Domino(i, j));
+            }
+        }
+
+        /*for(Dominoes dominoe : dominoes) {
+            System.out.println(dominoe.toChar(true));
+        }*/
+
+        StringJoiner sj = new StringJoiner("");
+        for (Domino domino : dominoList) {
+            sj.add(domino.toChar(false));
+        }
+        System.out.println(sj);
+    }
+
+    private static void testEquals() {
+        Domino domino1 = new Domino(3, 5);
+        Domino domino2 = new Domino(5, 3);
+        Domino domino3 = new Domino(3, 4);
+
+        boolean areEquals = domino1.equals(domino2);
+        System.out.println(domino1 + " es igual a " + domino2 + "? " + areEquals);
+        areEquals = domino1.equals(domino3);
+        System.out.println(domino1 + " es igual a " + domino3 + "? " + areEquals);
+    }
+
+    private static void testFlip() {
+        Domino domino = new Domino(3, 5);
+        System.out.println(domino);
+        domino = domino.flip();
+        System.out.println(domino);
+    }
+
+}
--- src/exercises/searching/SearchingDemo.java
+++ src/exercises/searching/SearchingDemo.java
@@ -1,37 +1,49 @@
 package exercises.searching;
 
-import exercises.model.Dominoes;
+import exercises.model.Domino;
 
 import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SearchingDemo {
 
     public static void main(String[] args) {
 
-        Dominoes[] dominoes = {
-                new Dominoes(1,5), // 0
-                new Dominoes(2,5), // 1
-                new Dominoes(3,4), // 2
-                new Dominoes(4,4), // 3
-                new Dominoes(0,6), // 4
-                new Dominoes(6,6), // 5
+        Domino[] dominoes = {
+                new Domino(1, 5), // 0
+                new Domino(2, 5), // 1
+                new Domino(3, 4), // 2
+                new Domino(4, 4), // 3
+                new Domino(0, 6), // 4
+                new Domino(6, 6), // 5
         };
 
         printDominoesVerticalFunctional(dominoes);
         printDominoesHorizontalFunctional(dominoes);
-        int result = indexOf(dominoes, new Dominoes(0,6));
-        System.out.println(result);
+
+        testFind(dominoes);
     }
 
-    static void printDominoesVertical(Dominoes[] dominoes) {
+    private static void testFind(Domino[] dominoes) {
+        Domino domino = new Domino(0, 6);
+        System.out.println("Buscando domino: " + domino + " en: " + Arrays.toString(dominoes));
+        int result = indexOfFunctional(dominoes, domino);
+        if (result == -1) {
+            System.out.println("Domino no encontrado");
+        } else {
+            System.out.println("Domino encontrado en indice: " + result);
+        }
+    }
+
+    static void printDominoesVertical(Domino[] dominoes) {
         for (int i = 0; i < dominoes.length; i++) {
             System.out.println(dominoes[i]);
         }
     }
 
-    static void printDominoesHorizontal(Dominoes[] dominoes) {
+    static void printDominoesHorizontal(Domino[] dominoes) {
         StringJoiner joiner = new StringJoiner(", ");
 
         for (int i = 0; i < dominoes.length; i++) {
@@ -41,27 +53,31 @@ public class SearchingDemo {
     }
 
 
-
-
-    static void printDominoesVerticalFunctional(Dominoes[] dominoes) {
+    static void printDominoesVerticalFunctional(Domino[] dominoes) {
         Arrays.stream(dominoes).forEach(System.out::println);
     }
 
-    static void printDominoesHorizontalFunctional(Dominoes[] dominoes) {
+    static void printDominoesHorizontalFunctional(Domino[] dominoes) {
         String text = Arrays.stream(dominoes)
-                .map(Dominoes::toString)
+                .map(Domino::toString)
                 .collect(Collectors.joining(", "));
         System.out.println(text);
     }
 
 
-
-    static int indexOf(Dominoes[] dominoes, Dominoes domino) {
+    static int indexOf(Domino[] dominoes, Domino domino) {
         for (int i = 0; i < dominoes.length; i++) {
             if (dominoes[i].equals(domino)) {
                 return i;
             }
         }
         return -1;
+    }
+
+    static int indexOfFunctional(Domino[] dominoes, Domino domino) {
+        return IntStream.range(0, dominoes.length)
+                .filter(i -> dominoes[i].equals(domino))
+                .findFirst()
+                .orElse(-1);
     }
 }
